7dcaa2ff1b0a1c4e798640c514253f43aba0e4c2
morioskouie9978 <99127301+morioskouie9978@users.noreply.github.com>
2022-10-17 16:23:02 +0330

    Add files via upload

---
+++ fuel-injection-perfection.py
@@ -0,0 +1,24 @@
+def solution(n):
+    # string to integer conversion
+    n = int(n)
+    # initializing the number of conversions
+    counter = 0
+    while(n != 1):
+        # indicating an exception for number 3 which results in two conversions
+        if(n == 3):
+            counter += 2
+            return counter
+        # the shortes pass is made by division. Unless it isn't divisble,
+        # we try to add or subtract it to check whether the new result is divisible by 4
+        # because 4 makes the path shorter than only 2
+        if(n % 2 == 0):
+            n //= 2
+            counter += 1
+        else:
+            if((n + 1) % 4 == 0):
+                n = ((n + 1) // 2)
+            else:
+                n = ((n - 1) // 2)
+            counter += 2
+    return counter
+print(solution(str(10**309-1)))
